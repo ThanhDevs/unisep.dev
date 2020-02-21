8489ecd88d851f9c14ffe6c5b8d326e192927687
Jessen <hcjtteam@gmail.com>
2020-02-21 11:47:39 +0700

    update_

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,10 +4,21 @@
     function uni_home_sl() {
         $('.uni_home_sl').slick();
     }
+
+    function our_team_sl() {
+        $('.our_team_sl').slick(
+            {
+                centerMode: true,
+                centerPadding: '60px',
+                slidesToShow: 3,
+            }
+        );
+    }
     
     //DOM Ready
     $(function() {
         uni_home_sl();
+        our_team_sl();
     });
 })();
 
