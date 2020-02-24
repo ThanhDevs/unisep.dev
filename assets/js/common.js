d67a27b4cff7324fa46527fb7c22a291cf396d46
Jessen <hcjtteam@gmail.com>
2020-02-25 01:16:37 +0700

    Add testimonial box

--- assets/js/common.js
+++ assets/js/common.js
@@ -14,11 +14,38 @@
             }
         );
     }
+
+    function modal_box() {
+        
+            $('.uni_testimonial_box .img_box').on('click', function() {
+                $('body').addClass('modal_show');
+            });
+
+            $('.modal_box').on('click', function() {
+                $('body').removeClass('modal_show');
+            });
+        
+    }
+
+    function testimonial_sl() {
+        $('.testimonial_sl').slick(
+            {
+                slidesToShow: 3,
+                easing: 'linear',
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                lazyLoad: 'ondemand',
+            }
+        );
+    }
     
     //DOM Ready
     $(function() {
         uni_home_sl();
         our_team_sl();
+        modal_box();
+        testimonial_sl();
     });
 })();
 
